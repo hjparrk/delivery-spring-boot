242868325663f2afc66513623ae7876f47ed9451
Hyojoon Park <hpar9758@naver.com>
2025-01-31 02:16:59 +1100

    feat: integrate RabbitMQ for order processing (Producer & Consumer)

---
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/order/consumer/OrderConsumer.java
@@ -0,0 +1,18 @@
+package org.delivery.storeadmin.domain.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.delivery.common.message.model.OrderMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderConsumer {
+
+    @RabbitListener(queues = "delivery.queue")
+    public void orderConsumer(OrderMessage orderMessage) {
+        log.info("message queue >>> {}", orderMessage);
+    }
+}
